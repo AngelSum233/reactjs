32ee98edd4b15103aba0cad391b1635eecc79ecd
AngelSum233 <1347486073@qq.com>
2021-02-08 13:07:50 +0800

    note

---
+++ example1-route/example4/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {
+    BroeserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    Redirect,
+    useHistory,
+    useLocation
+} from 'react-route-dom'
+
+const Example4 = ()=>{
+  return(
+    <Router></Router>
+  )
+}
